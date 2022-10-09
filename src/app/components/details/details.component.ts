aba9dcc57e8b561bff1ef2954a53fa47fc554fa7
whatsmysuggestion <whatsmysuggestion@gmail.com>
2022-10-09 18:47:54 +0530

    Angular Routing With Lazy Loading

---
+++ src/app/components/details/details.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+@Component({
+  selector: 'app-details',
+  template:`Details`
+})
+export class DetailsComponent   {
+
+ 
+}
---
+++ src/app/components/posts/posts.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-posts',
+   template:`Posts`
+})
+export class PostsComponent   {
+
+ 
+
+}
